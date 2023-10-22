3426f3e1b55ad7f17827f5888b95fa7cf98ce377
nafdev01 <naftaldev@gmail.com>
2023-10-22 14:54:53 +0300

    ...

--- django_project/settings.py
+++ django_project/settings.py
@@ -157,12 +157,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+if not LOCAL_SERVER:
+    from .cdn.conf import *
+
 STATIC_URL = "static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-if not LOCAL_SERVER:
-    from .cdn.conf import *
+
 # media files
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
