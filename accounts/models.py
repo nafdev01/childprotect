f2bdb1930af01b102fb2d5f712cd4aa1aa1c6a40
nafdev01 <naftaldev@gmail.com>
2023-10-22 19:00:31 +0300

    updated child avatar upload field

--- accounts/models.py
+++ accounts/models.py
@@ -18,7 +18,7 @@ def parent_profile_photo_path(instance, filename):
 
 def child_profile_photo_path(instance, filename):
     # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
-    return f"children/{instance.child.username}/avatars/{filename[-9:]}"
+    return f"children/{instance.child.username}/avatar/{filename[-9:]}"
 
 
 class ParentManager(UserManager):
