f1374c3333e6e412340ae2b9444ccd830db692e8
nafdev01 <naftaldev@gmail.com>
2023-10-22 14:47:58 +0300

    ...

--- django_project/cdn/backends.py
+++ django_project/cdn/backends.py
@@ -3,11 +3,11 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 class StaticRootS3Boto3Storage(S3Boto3Storage):
     location = "static"
-    file_overwrite = True
+    file_overwrite = False
 
 
 
 class MediaRootS3Boto3Storage(S3Boto3Storage):
     location = "media"
-    file_overwrite = True
+    file_overwrite = False
 
--- django_project/settings.py
+++ django_project/settings.py
@@ -161,8 +161,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "staticfiles")]
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
 # media files
