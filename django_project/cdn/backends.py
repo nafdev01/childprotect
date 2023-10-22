42242c5139587205597586e0d27fd41752bed6f2
nafdev01 <naftaldev@gmail.com>
2023-10-22 15:13:20 +0300

    ....

--- django_project/cdn/backends.py
+++ django_project/cdn/backends.py
@@ -6,8 +6,6 @@ class StaticRootS3Boto3Storage(S3Boto3Storage):
     file_overwrite = False
 
 
-
 class MediaRootS3Boto3Storage(S3Boto3Storage):
     location = "media"
     file_overwrite = False
-
--- django_project/cdn/conf.py
+++ django_project/cdn/conf.py
@@ -9,26 +9,11 @@ AWS_S3_ENDPOINT_URL = "https://fra1.digitaloceanspaces.com"
 
 AWS_S3_OBJECT_PARAMETERS = {
     "CacheControl": "max-age=86400",
+    "ACL": "public-read",
 }
 
 AWS_LOCATION = "https://childprotect.fra1.digitaloceanspaces.com"
 
-STORAGES = {
-    "default": {
-        "BACKEND": "django_project.cdn.backends.MediaRootS3Boto3Storage",
-    },
-    "staticfiles": {
-        "BACKEND": "django_project.cdn.backends.StaticRootS3Boto3Storage",
-    },
-}
 
-AWS_DEFAULT_ACL = None
-AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-
-STATIC_LOCATION = 'static'
-STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
-STATICFILES_STORAGE = 'hello_django.storage_backends.StaticStorage'
-# s3 public media settings
-PUBLIC_MEDIA_LOCATION = 'media'
-MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
-DEFAULT_FILE_STORAGE = 'hello_django.storage_backends.PublicMediaStorage'
+DEFAULT_FILE_STORAGE = "django_project.cdn.backends.MediaRootS3Boto3Storage"
+STATICFILES_STORAGE = "django_project.cdn.backends.StaticRootS3Boto3Storage"
