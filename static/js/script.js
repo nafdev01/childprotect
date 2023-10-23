fb9756e25d72d780186fcfa92521a1a6d6c79cc0
nafdev01 <naftaldev@gmail.com>
2023-10-23 07:20:13 +0300

    updated toggle password icons

--- static/js/script.js
+++ static/js/script.js
@@ -1,26 +1,43 @@
 // define contants for various variables
 const logPasswordField = document.querySelector("#id_password");
+const logPassField = document.querySelector("#id_pass");
 const signPasswordField = document.querySelector("#id_password1");
 const confirmSignPasswordField = document.querySelector("#id_password2");
 const signupForm = document.querySelector('signupForm'); // replace 'form' with the ID or class of your form element
 
 // // Function to toggle password visibility
-function togglePasswordVisibility(formType, checkBox) {
+function togglePasswordVisibility(formType, toggleIcon, passPosition = "") {
     if (formType == "login") {
-        if (checkBox.checked) {
-            logPasswordField.type = "text";
-        } else {
+        if (logPasswordField.type == "text" || logPassField.type == "text") {
             logPasswordField.type = "password";
+            logPassField.type = "password";
+            toggleIcon.innerHTML = '<i class="fa-regular fa-eye fa-lg"></i>';
+        } else {
+            logPasswordField.type = "text";
+            logPassField.type = "text";
+            toggleIcon.innerHTML = '<i class="fa-regular fa-eye-slash fa-lg"></i>';
         }
     } else if (formType == "signup") {
-        if (checkBox.checked) {
-            signPasswordField.type = "text";
-            confirmSignPasswordField.type = "text";
+        if (passPosition == "theFirst") {
+            if (signPasswordField.type == "text") {
+                signPasswordField.type = "password";
+                toggleIcon.innerHTML = '<i class="fa-regular fa-eye fa-lg"></i>';
+            }
+            else {
+                signPasswordField.type = "text";
+                toggleIcon.innerHTML = '<i class="fa-regular fa-eye-slash fa-lg"></i>';
+            }
         }
-        else {
-            signPasswordField.type = "password";
-            confirmSignPasswordField.type = "password";
-        }
+        else if (passPosition == "theSecond") {
+            if (confirmSignPasswordField.type == "text") {
+                confirmSignPasswordField.type = "password";
+                toggleIcon.innerHTML = '<i class="fa-regular fa-eye fa-lg"></i>';
+            }
+            else {
+                confirmSignPasswordField.type = "text";
+                toggleIcon.innerHTML = '<i class="fa-regular fa-eye-slash fa-lg"></i>';
+            }
 
+        }
     }
 }
