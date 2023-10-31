cc1713de36c6e38640b863fc3476fdd3c302eae9
nmatoya <naftaldev@gmail.com>
2023-10-31 18:28:53 +0300

    ....

--- forum/routing.py
+++ forum/routing.py
@@ -4,5 +4,5 @@ from forum.consumers import PostConsumer
 # Here, "" is routing to the URL ChatConsumer which
 # will handle the chat functionality.
 websocket_urlpatterns = [
-    path("", PostConsumer.as_asgi()),
+    path("ws/", PostConsumer.as_asgi()),
 ]
