768ab8e8d98f118bc50f95dc6059a0afc6a92681
nmatoya <naftaldev@gmail.com>
2023-10-31 12:01:51 +0300

    ...

--- django_project/asgi.py
+++ django_project/asgi.py
@@ -4,6 +4,7 @@ import os
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+from channels.security.websocket import AllowedHostsOriginValidator
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_project.settings")
@@ -13,10 +14,11 @@ application = get_asgi_application()
 
 from forum import routing
 
-
 application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
-        "websocket": AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns)),
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(routing.urlpatterns))
+        ),
     }
 )
