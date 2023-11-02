090de2910363313bd20728bff84d1aabf47b9442
nmatoya <naftaldev@gmail.com>
2023-11-02 10:59:19 +0300

    ....

--- awareness/views.py
+++ awareness/views.py
@@ -4,7 +4,7 @@ from accounts.decorators import child_required, parent_required
 from django.contrib.auth.decorators import login_required
 
 
-@child_required
+@child_required # restrict access to child only
 def intro(request):
     child = request.user
     child_profile = child.childprofile
