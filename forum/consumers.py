2bc369e0a80fbb931d3868a396f905fef56ce4de
nmatoya <naftaldev@gmail.com>
2023-11-03 17:41:51 +0300

    updated comments

--- forum/consumers.py
+++ forum/consumers.py
@@ -3,8 +3,9 @@ import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
 from accounts.models import User
-from forum.models import Post
+from forum.models import Post, Comment, TypeOfComment
 from django.utils.text import slugify
+from django.utils.timesince import timesince
 
 
 async def custom_save_post(text_data):
@@ -18,6 +19,27 @@ async def custom_save_post(text_data):
         await database_sync_to_async(post.save)()
         print(f"saved {post.title} by {post.created_by} to db")
         return post
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+async def custom_save_og_comment(text_data):
+    try:
+        text_data_json = json.loads(text_data)
+        username = text_data_json["username"]
+        content = text_data_json["content"]
+        post_id = text_data_json["post_id"]
+        parent = await database_sync_to_async(User.objects.get)(username=username)
+        post = await database_sync_to_async(Post.objects.get)(id=post_id)
+        comment = Comment(
+            comment_by=parent,
+            post=post,
+            content=content,
+            type_of_comment=TypeOfComment.ORIGINAL,
+        )
+        await database_sync_to_async(comment.save)()
+        print(f"saved {comment.comment_by}'s  og comment {comment.content[:20]} to db")
+        return comment
     except Exception as e:
         print(f"Error: {e}")
 
@@ -59,3 +81,52 @@ class PostConsumer(AsyncWebsocketConsumer):
         )
 
         await self.send(text_data=text_data)
+
+
+class OgCommentConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.roomGroupName = "group_og_comment"
+        await self.channel_layer.group_add(self.roomGroupName, self.channel_name)
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(self.roomGroupName, self.channel_layer)
+
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        comment = await custom_save_og_comment(text_data)
+        username = text_data_json["username"]
+        content = text_data_json["content"]
+
+        try:
+            await self.channel_layer.group_send(
+                self.roomGroupName,
+                {
+                    "type": "sendOgComment",
+                    "username": username,
+                    "content": content,
+                    "comment_id": comment.id,
+                    "comment_on": timesince(comment.comment_on),
+                },
+            )
+        except Exception as e:
+            print(e)
+
+    async def sendOgComment(self, event):
+        try:
+            username = event["username"]
+            content = event["content"]
+            comment_id = event["comment_id"]
+            comment_on = event["comment_on"]
+            text_data = json.dumps(
+                {
+                    "username": username,
+                    "content": content,
+                    "comment_id": comment_id,
+                    "comment_on": comment_on,
+                }
+            )
+
+            await self.send(text_data=text_data)
+        except Exception as e:
+            print(e)
--- forum/routing.py
+++ forum/routing.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from forum.consumers import PostConsumer
+from forum.consumers import PostConsumer, OgCommentConsumer
 
 # Here, "" is routing to the URL ChatConsumer which
 # will handle the chat functionality.
 websocket_urlpatterns = [
     path("ws/", PostConsumer.as_asgi()),
+    path("ws/og/comment/", OgCommentConsumer.as_asgi()),
 ]
